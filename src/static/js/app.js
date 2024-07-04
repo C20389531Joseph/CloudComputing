4d1e11c9baf747ef9b027a3db83a4f5299fa3e03
Joseph <C20389531@mytudublin.ie>
2024-07-04 10:33:59 +0100

    Changed text

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="A Creative New Item please"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add a new Item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
